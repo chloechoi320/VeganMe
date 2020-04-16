c0d542a3d38879e483959cb24c861cd0e0016cf5
= <=>
2020-04-15 23:50:02 -0700

    first commit - finished forms

---
+++ comps/CustomForm/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './customform.css';
+
+const CustomForm = ({text, color}) => 
+<div>
+    <input 
+    style = {
+        {borderBottom: "solid 1px" + color}
+    }
+        className="form_box"
+        type={text} 
+        placeholder={text}/>
+    <p><b>{text}</b></p>
+</div>
+
+CustomForm.defaultProps = {
+    text: "Your name",
+    color: "#789174"
+};
+
+export default CustomForm;
---
+++ stories/CustomForms.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CustomForm from '../comps/CustomForm';
+
+export default {
+    title: "Custom Form",
+    component: MyCustomForm
+}
+
+export const MyCustomForm = () => <CustomForm/>
