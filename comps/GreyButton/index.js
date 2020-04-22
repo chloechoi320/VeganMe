13925efcc04736c2ae996535e23e20a6cbbfd56f
= <=>
2020-04-22 14:27:14 -0700

    made grey buttons

---
+++ comps/GreyButton/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './greybutton.css';
+
+const GreyButton = ({text, onClick}) => <div
+className="grey-btn"
+onClick={onClick}>
+    <div className="grey-btn-icon"></div>
+    <div className="grey-btn-label">{text}</div>
+</div>
+
+GreyButton.defaultProps = {
+    text: "Label Name"
+}
+
+export default GreyButton;
