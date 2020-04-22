30cd4e0faa2789f4031b0adee8d15e35e46b123c
= <=>
2020-04-21 23:50:36 -0700

    installed icons, next.js, finished favbox comp

--- comps/FavouritesBox/index.js
+++ comps/FavouritesBox/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Header from '../Header'
 import './favouritesbox.css';
+import { FaStar } from 'react-icons/fa'
 
 const FavouritesBox = () => 
 
 <div className="favourites_container">
     <div className="favourites_image"></div>
-    <div className="favourite_icon"></div>
+    <div className="favourites_icon">
+        <FaStar color="#FFF" size="1.5em"/>
+    </div>
+    <div className="favourites_header_container">
+        <Header fontSize={11}/>
+    </div>
 </div>
 
 FavouritesBox.defaultProps = {
