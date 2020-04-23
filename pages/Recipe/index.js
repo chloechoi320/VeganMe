120eb6c8fa3669af550c019e81781134cddfc0b6
= <=>
2020-04-23 04:14:09 -0700

    welcome+recipe completed; need to insert grey icon

---
+++ pages/Recipe/index.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from "react";
+import "./recipe.css";
+
+import BackHeader from "../../comps/BackHeader";
+import Header from "../../comps/Header";
+import RecipeIngredients from "../../comps/RecipeIngredients";
+import RecipeInstructions from "../../comps/RecipeInstructions";
+import GreyButton from "../../comps/GreyButton";
+import NavigationBar from "../../comps/NavigationBar";
+
+import {FaStar} from 'react-icons/fa';
+import {IoIosArrowRoundUp} from 'react-icons/io';
+
+const RecipePage = () => {
+    return <div className="recipe-pg">
+        <BackHeader />
+        <div className="rp-container">
+            <Header text="Recipe Name" fontSize={14}/>
+            <div className="rp-details">
+                <div>Author: (author) Prep Time: (min)</div>
+                <div>Cook Time: (min) Category: (category)</div>
+            </div>
+            <img src="https://www.solidbackgrounds.com/images/2560x1440/2560x1440-teal-blue-solid-color-background.jpg" />
+            <div className="rp-favourite">
+                <div className="fav-icon"><FaStar size="15px" color="#fff" /></div>
+                <div className="fav-text">Add to Favourites</div>
+            </div>
+            <div className="rp-desc">Recipe description.</div>
+            <RecipeIngredients />
+            <div className="line"></div>
+            <Header text="Insturctions" fontSize={14} />
+            <RecipeInstructions />
+            <img src="https://www.solidbackgrounds.com/images/2560x1440/2560x1440-teal-blue-solid-color-background.jpg" />
+            <div className="gry-btn">
+                <GreyButton text="Back to Top"><IoIosArrowRoundUp color="#1e1d1d" />
+                </GreyButton>
+            </div>
+        </div>
+        <div className="nav-bar">
+            <NavigationBar />
+        </div>
+    </div>
+}
+
+export default RecipePage;
