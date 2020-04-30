05ce36ad38c1114e2395a0719002080376980e90
= <=>
2020-04-30 00:17:37 -0700

    stylised recipe and favouries

--- comps/FavouritesBox/index.js
+++ comps/FavouritesBox/index.js
@@ -3,10 +3,10 @@ import Header from '../Header'
 import './favouritesbox.css';
 import { FaStar } from 'react-icons/fa'
 
-const FavouritesBox = ({text}) => 
+const FavouritesBox = ({text, img}) => 
 
 <div className="favourites_container">
-    <div className="favourites_image"></div>
+    <div style={{backgroundImage:img}}className="favourites_image"></div>
     <div className="favourites_icon">
         <FaStar color="#FFF" size="1.5em"/>
     </div>
@@ -16,7 +16,8 @@ const FavouritesBox = ({text}) =>
 </div>
 
 FavouritesBox.defaultProps = {
-    text: "Meet on Main"
+    text: "Meet on Main",
+    img: 'url("https://images.unsplash.com/photo-1494859802809-d069c3b71a8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")'
 };
 
 export default FavouritesBox;
