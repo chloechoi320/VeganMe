30bd4bffede5738602a9d0f54c2058a5c5506d81
= <=>
2020-05-01 16:30:27 -0700

    fixed icons, added fav / unfav feature

--- pages/VeganPizzaHouse/index.js
+++ pages/VeganPizzaHouse/index.js
@@ -16,12 +16,12 @@ const VeganPizzaHousePage = () => {
             </div>
             <div className="vph-img"></div>
             <div className="adr">
-                <div className="adr-head">Adress</div>
+                <div className="adr-head">Address</div>
                 <div>2119 Kingsway, Vancouver, BC V5N 2T4</div>
             </div>
             <div className="desc">
                 <p>Vegan pizzeria offering pies, slices, pasta dishes and salads with house-made sauces and dressings.</p>
-                <p>for more info, visit <a href="http://www.veganpizzahouse.ca/">veganpizzahouse.ca</a></p>
+                <p>For more info, visit <a href="http://www.veganpizzahouse.ca/">veganpizzahouse.ca</a></p>
             </div>
         </div>
         <div className="nav-bar">
