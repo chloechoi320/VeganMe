3d1e2855512bcbb0810026872005a9834d2df605
= <=>
2020-05-01 22:29:43 -0700

    back button works yeeee

--- comps/BackHeader/index.js
+++ comps/BackHeader/index.js
@@ -3,11 +3,16 @@ import Header from '../Header';
 import './backheader.css';
 import { IoIosArrowBack } from 'react-icons/io';
 
+function GoBack() {
+    window.history.back();
+}
+
 const BackHeader = () =>
+
 
 <div className="bh_container">
     <div className="bh_arrow"><IoIosArrowBack size="1.25em" color="#1E1D1D"/></div>
-    <div><Header color="#1E1D1D" fontSize={10} text="Back"/></div>
+    <div><Header color="#1E1D1D" fontSize={10} text="Back" onClick={GoBack}/></div>
 </div>
 
 export default BackHeader; 
