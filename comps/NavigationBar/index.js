38ce03ce39a3eef25664c1309eca26606db92774
= <=>
2020-05-02 00:46:45 -0700

    Re-stylize bottom nav bar

--- comps/NavigationBar/index.js
+++ comps/NavigationBar/index.js
@@ -2,33 +2,45 @@ import React from 'react';
 import './navigationbar.css';
 import Link from 'next/link';
 import Router from 'next/router';
-
-function ClickMap() {
-    document.querySelector("#map").style.right = "-100%";
-    Router.push("/Locations");
-}
+import {FiMapPin} from 'react-icons/fi';
+import {FiHome}  from 'react-icons/fi';
+import {GoGear} from 'react-icons/go';
+import {FaRegStar} from 'react-icons/fa';
 
 function ClickHome() {
     document.querySelector("#home").style.right = "-100%";
     Router.push("/Home");
 }
 
-function ClickRecources() {
-    document.querySelector("#resources").style.right = "-100%";
-    Router.push("/Resources");
+function ClickMap() {
+    document.querySelector("#map").style.right = "-100%";
+    Router.push("/Locations");
+}
+
+function ClickStar() {
+    document.querySelector("#star").style.right = "-100%";
+    Router.push("/Favourites")
+}
+
+function ClickGear() {
+    document.querySelector("#gear").style.right = "-100%";
+    Router.push("/Settings");
 }
 
 const NavigationBar = () => <div>
     <nav class="bar">
         <div class="container">
-            <div id="map" onClick={ClickMap}>
-                <img src={require('./map.png')}></img>
-            </div>
             <div id="home" onClick={ClickHome}>
-                <img src={require('./home.png')}></img>
+                <FiHome size="1.8em"/>
             </div>
-            <div id="resources" onClick={ClickRecources}>
-                <img src={require('./resources.png')}></img>
+            <div id="map" onClick={ClickMap}>
+                <FiMapPin size="1.8em"/>
+            </div>
+            <div id="star" onClick={ClickStar}>
+                <FaRegStar size="1.8em"/>
+            </div>
+            <div id="gear" onClick={ClickGear}>
+                <GoGear size="1.8em"/>
             </div>
         </div>
     </nav>
