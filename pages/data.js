d143e3ea53a7ea298fe77ef20cecbda79d5f7ffe
= <=>
2020-05-13 14:27:02 -0700

    playing around with data :))

--- pages/data.js
+++ pages/data.js
@@ -5,11 +5,6 @@ export var data = {
     veganismBudget: ""
 }
 
-if (process.browser) {
-    var sessiondata = sessionStorage.getItem("data");
-    changeData(JSON.parse(sessiondata));
-}
-
 export function changeData(d) {
     data = d;
     sessionStorage.setItem("data", JSON.stringify(data));
